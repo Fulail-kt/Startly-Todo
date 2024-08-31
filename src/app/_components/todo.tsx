9aeed96255e11f90166760afb42b72938ea005bf
fulail-kt <muhamedfulail77@gmail.com>
2024-08-31 12:27:26 +0530

    minor updates

--- src/app/_components/todo.tsx
+++ src/app/_components/todo.tsx
@@ -22,26 +22,37 @@ interface TodoInput {
 }
 
 export function Todo() {
+
+  // custom hook for all api
   const { createTodo, updateTodo, deleteTodo, completeTodo } = useTodoApi();
   const [getAllTodo] = api.todo.getAll.useSuspenseQuery();
   const utils = api.useUtils();
 
-  const [todo, setTodo] = useState<TodoInput>({ title: '', description: '' });
+  const [todo, setTodo] = useState<TodoInput>({ title: "", description: "" });
   const [editId, setEditId] = useState<number | null>(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedTodoId, setSelectedTodoId] = useState<number | null>(null);
 
-  const handleEdit = (todoId: number, todoTitle: string, todoDescription: string) => {
+
+  //edit function
+  const handleEdit = (
+    todoId: number,
+    todoTitle: string,
+    todoDescription: string,
+  ) => {
     setTodo({ title: todoTitle, description: todoDescription });
     setEditId(todoId);
   };
 
+  // delete function
   const handleDelete = (todoId: number) => {
     setSelectedTodoId(todoId);
     setIsModalOpen(true);
   };
 
-  const handleComplete = (todoId: number, status: boolean) => {
+
+  // complete function
+    const handleComplete = (todoId: number, status: boolean) => {
     setSelectedTodoId(todoId);
     completeTodo.mutate({
       id: todoId,
@@ -49,20 +60,24 @@ export function Todo() {
     });
   };
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+
+  // storing the value to todo state
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
     const { name, value } = e.target;
-    setTodo((prevTodo: {title:string,description:string}) => ({
+    setTodo((prevTodo: { title: string; description: string }) => ({
       ...prevTodo,
-      [name]: value
+      [name]: value,
     }));
   };
-
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const trimmedTitle = todo.title.trim();
     const trimmedDescription = todo.description.trim();
 
+    // trimming the string to remove unwanted white space
     if (trimmedTitle === "") {
       toast.error("Title cannot be empty.");
       return;
@@ -73,56 +88,62 @@ export function Todo() {
     }
 
     const todoExists = getAllTodo.some(
-      (item: Todo) => item.title !== null && item.title.trim().toLowerCase() === trimmedTitle.toLowerCase()
+      (item: Todo) =>
+        item.title !== null &&
+        item.title.trim().toLowerCase() === trimmedTitle.toLowerCase(),
     );
 
     if (editId) {
       const existingTodo = getAllTodo.find((item: Todo) => item?.id === editId);
-      if (!todoExists || (existingTodo && existingTodo.title === trimmedTitle)) {
-        updateTodo.mutate({ 
-          id: editId, 
-          title: trimmedTitle, 
-          description: trimmedDescription 
+      if (
+        !todoExists ||
+        (existingTodo && existingTodo.title === trimmedTitle)
+      ) {
+        updateTodo.mutate({
+          id: editId,
+          title: trimmedTitle,
+          description: trimmedDescription,
         });
         setEditId(null);
-        setTodo({ title: '', description: '' });
+        setTodo({ title: "", description: "" });
       } else {
         toast.error("Todo with this title already exists.");
       }
     } else {
       if (!todoExists) {
-        createTodo.mutate({ 
-          title: trimmedTitle, 
-          description: trimmedDescription 
+        createTodo.mutate({
+          title: trimmedTitle,
+          description: trimmedDescription,
         });
-        setTodo({ title: '', description: '' });
+        setTodo({ title: "", description: "" });
       } else {
         toast.error("Todo with this title already exists.");
       }
     }
   };
   return (
-    <div className="border-1 shadow-2xl shadow-violet-900 max-h-[560px]  w-full max-w-md rounded-md border border-violet-900 p-6">
-      <form onSubmit={handleSubmit} className="flex flex-col gap-2 ">
+    <div className="border-1 max-h-[560px] w-full max-w-md rounded-md border border-violet-900 p-6 shadow-2xl shadow-violet-900">
+      {/* form for adding todo */}
+      <form onSubmit={handleSubmit} className="flex flex-col gap-2">
         <div className="flex items-center justify-around gap-x-6">
-        <div className="flex flex-col gap-2">
-          <input
-            type="text"
-            name="title"
-            placeholder="Title"
-            value={todo.title}
-            onChange={handleInputChange}
-            className="w-full rounded-md px-4 py-3 text-black"
-          />
-          <textarea
-            name="description"
-            placeholder="Description"
-            value={todo.description}
-            onChange={handleInputChange}
-            className="w-full rounded-md px-4 py-3 text-black"
-            rows={1}
-          />
-        </div>
+          <div className="flex flex-col gap-2">
+            <input
+              type="text"
+              name="title"
+              placeholder="Title"
+              value={todo.title}
+              onChange={handleInputChange}
+              className="w-full rounded-md px-4 py-3 text-black"
+            />
+            <textarea
+              name="description"
+              placeholder="Description"
+              value={todo.description}
+              onChange={handleInputChange}
+              className="w-full rounded-md px-4 py-3 text-black"
+              rows={1}
+            />
+          </div>
           <button
             type="submit"
             className="flex items-center justify-center rounded-md bg-white/10 px-6 py-3 font-semibold transition hover:bg-white/20"
@@ -138,69 +159,80 @@ export function Todo() {
           </button>
         </div>
       </form>
-      <div className="max-h-80 w-full no-scroll mt-2 overflow-y-auto ">
-        
+      <div className="no-scroll mt-2 max-h-80 w-full overflow-y-auto">
         {/* Display todos */}
-        { !getAllTodo?.length ? (
+        {!getAllTodo?.length ? (
           <div className="mt-4 flex w-full items-center justify-center gap-3 rounded bg-gray-400/50 px-3">
-            <h2 className="text-lg italic py-2.5">Add Some Todos</h2>
+            <h2 className="py-2.5 text-lg italic">Add Some Todos</h2>
           </div>
-        ) :      
-        getAllTodo?.map((todo:Todo) => (
-          <div
-            className="text-break-full mt-4 flex w-full items-center justify-between gap-3 rounded bg-gray-400/50 px-3"
-            key={todo?.id}
-          >
-            <span className="w-full rounded-md py-3">
-              <div className="flex justify-between items-center">
-              <h2
-                  className={` ${todo?.completed ? "text-green-500" : ""} text-lg break-all`}
-                >
-                  {todo?.title}
-                </h2>
-                <span className="w-[10%]">
-                  <Tooltip content={ <div className="flex flex-col justify-between gap-3 ">
-                              <span className="text-xs"> {todo?.createdAt.toLocaleDateString()}</span>
-                              <span>Description : {todo?.description}</span>
-                            </div>}>
-                  <span className="flex rounded-full border items-center px-1.5 text-xs  font-light text-white bg-transparent ">
-                            i
+        ) : (
+          getAllTodo?.map((todo: Todo) => (
+            <div
+              className="text-break-full mt-4 flex w-full items-center justify-between gap-3 rounded bg-gray-400/50 px-3"
+              key={todo?.id}
+            >
+              <span className="w-full rounded-md py-3">
+                <div className="flex items-center justify-between">
+                  <h2
+                    className={` ${todo?.completed ? "text-green-500" : ""} break-all text-lg`}
+                  >
+                    {todo?.title}
+                  </h2>
+                  {/* tool tip for the description */}
+                  <span className="w-[10%]">
+                    <Tooltip
+                      content={
+                        <div className="flex flex-col justify-between gap-3">
+                          <span className="text-xs">
+                            {" "}
+                            {todo?.createdAt.toLocaleDateString()}
                           </span>
-                        </Tooltip>
-                </span>
-              </div>
-            </span>
-            <span className="w-30 flex justify-center gap-2">
-              <span
-                className="rounded-md bg-gray-300 p-2"
-                onClick={() => handleEdit(todo?.id, todo?.title!,todo?.description!)}
-              >
-                <Edit2 color="blue" size={18} />
+                          <span>Description : {todo?.description}</span>
+                        </div>
+                      }
+                    >
+                      <span className="flex items-center rounded-full border bg-transparent px-1.5 text-xs font-light text-white">
+                        i
+                      </span>
+                    </Tooltip>
+                  </span>
+                </div>
               </span>
-              <span
-                className="rounded-md bg-gray-300 p-2"
-                onClick={() => handleDelete(todo?.id)}
-              >
-                <Trash color="red" size={18} />
-              </span>
-              {todo?.completed ? (
+              {/* Action buttons */}
+              <span className="w-30 flex justify-center gap-2">
                 <span
                   className="rounded-md bg-gray-300 p-2"
-                  onClick={() => handleComplete(todo?.id, false)}
+                  onClick={() =>
+                    handleEdit(todo?.id, todo?.title!, todo?.description!)
+                  }
                 >
-                  <Undo2 color="orange" size={18} />
+                  <Edit2 color="blue" size={18} />
                 </span>
-              ) : (
                 <span
                   className="rounded-md bg-gray-300 p-2"
-                  onClick={() => handleComplete(todo?.id, true)}
+                  onClick={() => handleDelete(todo?.id)}
                 >
-                  <Check color="green" size={18} />
+                  <Trash color="red" size={18} />
                 </span>
-              )}
-            </span>
-          </div>
-        ))}
+                {todo?.completed ? (
+                  <span
+                    className="rounded-md bg-gray-300 p-2"
+                    onClick={() => handleComplete(todo?.id, false)}
+                  >
+                    <Undo2 color="orange" size={18} />
+                  </span>
+                ) : (
+                  <span
+                    className="rounded-md bg-gray-300 p-2"
+                    onClick={() => handleComplete(todo?.id, true)}
+                  >
+                    <Check color="green" size={18} />
+                  </span>
+                )}
+              </span>
+            </div>
+          ))
+        )}
       </div>
 
       {/* Modal for confirmation */}
